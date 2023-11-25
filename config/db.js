048d04845d166ea193e06fbb05a7193bf194d939
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2023-11-25 15:06:25 +0530

    commit

--- config/db.js
+++ config/db.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const { updateTrackPrices } = require("../service/track.service");
 require("dotenv").config();
 
 mongoose.connect(process.env.MONGOURL, {
@@ -12,7 +11,6 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 db.once("open", () => {
   console.log("Connected to MongoDB");
-  updateTrackPrices();
 });
 
 module.exports = mongoose;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const cron = require("node-cron");
+const { updateTrackPrices } = require("./service/track.service");
 require("./config/db");
 
 const app = express();
@@ -8,6 +9,19 @@ const app = express();
 const PORT = 3000;
 
 app.use(cors());
+
+cron.schedule(
+  "0 */3 * * *",
+  function () {
+    console.log("---------------------");
+    console.log("Updating tracking prices on every 3 hours");
+    updateTrackPrices();
+  },
+  {
+    scheduled: true,
+    timezone: "Asia/Kolkata",
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`Listening to the PORT : ` + PORT);
